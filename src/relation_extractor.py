ccb64152a33316da1026a7857174abc22f720430
nevaeh0603 <singhnevaeh0603@gmail.com>
2026-06-05 20:24:12 +0530

    Added Relation Extractor

---
+++ src/relation_extractor.py
@@ -0,0 +1,29 @@
+import spacy
+nlp= spacy.load("en_core_web_sm")
+
+def extract_relation(text):
+    doc=nlp(text)
+    relation=[]
+
+    for sent in doc.sents:
+        subject=None
+        relation=None
+        object=None
+
+        for token in sent:
+            #Subject
+            if token.dep_ == "nsubj":
+                subject = token.text
+
+            # Verb
+            if token.pos_ == "VERB":
+                relation = token.lemma_
+
+            # Object
+            if token.dep_ in ["dobj", "pobj"]:
+                object_ = token.text
+
+        if subject and relation and object_:
+            relation.append((subject, relation, object_))
+            
+    return relation
